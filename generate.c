d857094772c6cde3f2195e78885d00d20e245bf2
laim0nas100 <aciukadsiunciate@gmail.com>
2017-05-23 22:58:06 +0300

    update

--- generate.c
+++ generate.c
@@ -4,10 +4,16 @@ int main(int argc, char** argv) {
   FILE *f = fopen("run","w");
   int computerCount = atoi(argv[1]);
   int procCount = atoi(argv[2]);
+  
   fprintf(f,"#!/bin/sh \n");
   fprintf(f,"#SBATCH -p short\n");
   fprintf(f,"#SBATCH -N%d\n",computerCount);
   fprintf(f,"#SBATCH -c%d\n",procCount);
+  char* cluster = "alpha";
+  if(argc == 4){
+    cluster = argv[3];
+  }
+  fprintf(f,"#SBATCH -C %s\n",cluster);
   fprintf(f,"make run np=%d\n",procCount);
   fclose(f);
   
